f36e3ae2da4c1956774f3c9efe7ae730c4e1e0e4
T-foseraw <tfoserawsrl@gmail.com>
2023-11-23 02:09:30 -0400

    Save

---
+++ js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    
+  $('#btn_rct_b1').click(function () {
+
+    $('#batido_4').fadeOut();
+    $('#batido_3').fadeOut();
+    $('#batido_2').fadeOut();
+    $('#batido_1').fadeIn();
+    
+  });
+
+  $('#btn_rct_b2').click(function () {
+
+    $('#batido_4').fadeOut();
+    $('#batido_3').fadeOut();
+    $('#batido_1').fadeOut();
+    $('#batido_2').fadeIn();
+    
+  });
+
+  $('#btn_rct_b3').click(function () {
+
+    $('#batido_4').fadeOut();
+    $('#batido_1').fadeOut();
+    $('#batido_2').fadeOut();
+    $('#batido_3').fadeIn();
+    
+  });
+  $('#btn_rct_b4').click(function () {
+
+    $('#batido_1').fadeOut();
+    $('#batido_3').fadeOut();
+    $('#batido_2').fadeOut();
+    $('#batido_4').fadeIn();
+    
+  });
+
+
+
+  $('#btn_rct_d1').click(function () {
+
+    $('#desayuno_3').fadeOut();
+    $('#desayuno_2').fadeOut();
+    $('#desayuno_1').fadeIn();
+    
+  });
+  $('#btn_rct_d2').click(function () {
+
+    $('#desayuno_3').fadeOut();
+    $('#desayuno_1').fadeOut();
+    $('#desayuno_2').fadeIn();
+    
+  });
+  $('#btn_rct_d3').click(function () {
+
+    $('#desayuno_1').fadeOut();
+    $('#desayuno_2').fadeOut();
+    $('#desayuno_3').fadeIn();
+    
+  });
+
+
+
+  $('#btn_rct_p1').click(function () {
+
+    $('#postre_3').fadeOut();
+    $('#postre_2').fadeOut();
+    $('#postre_1').fadeIn();
+    
+  });
+  $('#btn_rct_p2').click(function () {
+
+    $('#postre_3').fadeOut();
+    $('#postre_1').fadeOut();
+    $('#postre_2').fadeIn();
+    
+  });
+  $('#btn_rct_p3').click(function () {
+
+    $('#postre_1').fadeOut();
+    $('#postre_2').fadeOut();
+    $('#postre_3').fadeIn();
+    
+  });
+
+
+
+
+});
